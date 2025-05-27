81531d6d9ce8f0eda304588b9f0e3ca6f0717912
Den41k62 <deniskoledin.rb@gmail.com>
2025-05-27 17:46:26 +0500

    Delete todotomorrow2 directory

--- todotomorrow2/app/src/main/java/com/example/todotomorrow/database/TaskDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.todotomorrow.database;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Task.class}, version = 1)
-public abstract class TaskDatabase extends RoomDatabase {
-    public abstract TaskDao taskDao();
-}
